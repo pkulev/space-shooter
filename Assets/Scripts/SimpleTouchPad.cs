211543f9f850eb359ff371389e0e76d9140d80eb
Pavel Kulyov <kulyov.pavel@gmail.com>
2020-02-08 17:41:45 +0300

    add mobile things

---
+++ Assets/Scripts/SimpleTouchPad.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SimpleTouchPad : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler {
+
+    public float smoothing;
+
+    private Vector2 origin;
+    private Vector2 direction;
+    private Vector2 smoothDirection;
+    private bool touched;
+    private int pointerID;
+
+    void Awake() {
+        direction = Vector2.zero;
+        touched = false;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        if (eventData.pointerId == pointerID) {
+            Vector2 currentPosition = eventData.position;
+            direction = (currentPosition - origin).normalized;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if (!touched) {
+            touched = true;
+            pointerID = eventData.pointerId;
+            origin = eventData.position;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        if (eventData.pointerId == pointerID) {
+            direction = Vector2.zero;
+            touched = false;
+        }
+    }
+
+    public Vector2 GetDirection() {
+        smoothDirection = Vector2.MoveTowards(smoothDirection, direction, smoothing);
+        return smoothDirection;
+    }
+}
---
+++ Assets/Scripts/TouchArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TouchArea : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+    private bool touched;
+    private int pointerID;
+    private bool canFire;
+
+    void Awake() {
+        touched = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if (!touched) {
+            touched = true;
+            pointerID = eventData.pointerId;
+            canFire = true;
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        if (eventData.pointerId == pointerID) {
+            canFire = false;
+            touched = false;
+        }
+    }
+
+    public bool CanFire() {
+        return canFire;
+    }
+}
